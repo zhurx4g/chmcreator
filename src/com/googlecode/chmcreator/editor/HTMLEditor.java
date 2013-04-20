ff2372c6d78cd7fc7b217b11987be00108591e2d
zhurx4g <zhurx4g@gmail.com>
2013-04-20 14:39:04 +0800

    high light

--- src/com/googlecode/chmcreator/editor/HTMLEditor.java
+++ src/com/googlecode/chmcreator/editor/HTMLEditor.java
@@ -1,11 +1,19 @@
 package com.googlecode.chmcreator.editor;
 
 import java.awt.Cursor;
-import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.Bullet;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.custom.LineStyleEvent;
+import org.eclipse.swt.custom.LineStyleListener;
+import org.eclipse.swt.custom.ST;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.GlyphMetrics;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.FormAttachment;
@@ -14,19 +22,23 @@ import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.CoolBar;
 import org.eclipse.swt.widgets.CoolItem;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.eclipsian.swt.composer.HyperComposer;
-import org.eclipsian.swt.composer.IURIResolver;
 
 import com.googlecode.chmcreator.ResourceLoader;
 
 public class HTMLEditor extends Composite {
 	private CTabFolder folder;
-	private Text text;
+	private StyledText styledText;
 	private HyperComposer composer;
 	private String path = "";
+	private HTMLLineStyler lineStyler = new HTMLLineStyler();
+	final static List<String> KEYWORDS = new ArrayList<String>();
+	static {
+		KEYWORDS.add("<html>");
+		KEYWORDS.add("</html>");
+	}
 	public HTMLEditor(Composite parent, int arg1) {
 		super(parent, arg1);
 		this.setLayout(new FillLayout());
@@ -34,8 +46,23 @@ public class HTMLEditor extends Composite {
 		
 		CTabItem html = new CTabItem(folder, SWT.NORMAL);
 		html.setText("HTML");
-		text = new Text(folder, SWT.BORDER|SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
-		html.setControl(text);
+		styledText = new StyledText(folder, SWT.BORDER|SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
+		styledText.addLineStyleListener(lineStyler);
+//		styledText.addLineStyleListener(new LineStyleListener()
+//		{
+//		    public void lineGetStyle(LineStyleEvent e)
+//		    {
+//		    	int lineNumber = styledText.getLineAtOffset(e.lineOffset);
+//		        //Set the line number
+//		        e.bulletIndex = lineNumber;
+//		        //Set the style, 12 pixles wide for each digit
+//		        StyleRange style = new StyleRange();
+//		        style.metrics = new GlyphMetrics(0, 0, Integer.toString(styledText.getLineCount()+1).length()*12);
+//		        //Create and set the bullet
+//		        e.bullet = new Bullet(ST.BULLET_NUMBER,style);
+//		    }
+//		});
+		html.setControl(styledText);
 		
 		CTabItem design = new CTabItem(folder, SWT.NORMAL);
 		design.setText("Design");
@@ -99,7 +126,8 @@ public class HTMLEditor extends Composite {
 	}
 
 	public void setContent(String content){
-		text.setText(content);
+		//lineStyler.parseBlockComments(content);
+		styledText.setText(content);
 		composer.setContent(content);
 	}
 	ToolItem newToolItem(ToolBar toolBar, String imageName, String toolTip){
