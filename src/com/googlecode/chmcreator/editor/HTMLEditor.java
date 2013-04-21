208e5a847e9d8aed51df711ea5f0aafdc28bf14a
zhurx4g <zhurx4g@gmail.com>
2013-04-21 21:27:03 +0800

    editor

--- src/com/googlecode/chmcreator/editor/HTMLEditor.java
+++ src/com/googlecode/chmcreator/editor/HTMLEditor.java
@@ -13,6 +13,7 @@ import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Point;
@@ -26,6 +27,7 @@ import org.eclipse.swt.widgets.CoolItem;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
+import org.eclipsian.swt.composer.EditorCommand;
 import org.eclipsian.swt.composer.HyperComposer;
 import org.eclipsian.swt.composer.IDocumentStateListener;
 
@@ -84,31 +86,78 @@ public class HTMLEditor extends Composite {
 		Composite parentComposite = new Composite(folder, SWT.NORMAL);
 		parentComposite.setLayout(new FormLayout());
 		
+		SelectionListener listener = new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				composer.execCommand((EditorCommand) e.widget.getData());
+				updateState(State.WYSWYG_MODIFY, false);
+			}
+		};
+		
 		CoolBar coolBar = new CoolBar(parentComposite, SWT.NORMAL);
 		ToolBar toolBar = new ToolBar(coolBar, SWT.FLAT);
-		ToolItem newItem1 = newItem(toolBar, "editor/bold.gif","New");
-		ToolItem newItem2 = newItem(toolBar, "editor/italic.gif","New");
-		ToolItem newItem3 = newItem(toolBar, "editor/underline.gif","New");
-		ToolItem newItem4 = newItem(toolBar, "editor/fgcolor.gif","New");
-		ToolItem newItem5 = newItem(toolBar, "editor/hlcolor.gif","New");//bgcolor
-		ToolItem newItem6 = newItem(toolBar, "editor/bgcolor.gif","New");
+		ToolItem newItem1 = newItem(toolBar, "editor/bold.gif","Bold");
+		newItem1.setData(HyperComposer.CMD_BOLD);
+		newItem1.addSelectionListener(listener);
 		
-		ToolItem newItem7 = newItem(toolBar, "editor/alignleft.gif","New");
-		ToolItem newItem8 = newItem(toolBar, "editor/aligncenter.gif","New");
-		ToolItem newItem9 = newItem(toolBar, "editor/alignright.gif","New");
-		ToolItem newItem10 = newItem(toolBar, "editor/alignjustify.gif","New");
+		ToolItem newItem2 = newItem(toolBar, "editor/italic.gif","Italic.gif");
+		newItem2.setData(HyperComposer.CMD_ITALIC);
+		newItem2.addSelectionListener(listener);
 		
-		ToolItem newItem11 = newItem(toolBar, "editor/indent.gif","New");
-		ToolItem newItem12 = newItem(toolBar, "editor/outdent.gif","New");
+		ToolItem newItem3 = newItem(toolBar, "editor/underline.gif","Underline.gif");
+		newItem3.setData(HyperComposer.CMD_UNDERLINE);
+		newItem3.addSelectionListener(listener);
 		
-		ToolItem newItem13 = newItem(toolBar, "editor/orderlist.gif","New");
-		ToolItem newItem14 = newItem(toolBar, "editor/unorderlist.gif","New");
+		ToolItem newItem4 = newItem(toolBar, "editor/fgcolor.gif","fg color");
+		//newItem4.setData(HyperComposer.CMD_);
+		ToolItem newItem5 = newItem(toolBar, "editor/hlcolor.gif","hl color");//bgcolor
+		ToolItem newItem6 = newItem(toolBar, "editor/bgcolor.gif","bg color");
 		
-		ToolItem newItem15 = newItem(toolBar, "editor/subscript.gif","New");
-		ToolItem newItem16 = newItem(toolBar, "editor/superscript.gif","New");
+		ToolItem newItem7 = newItem(toolBar, "editor/alignleft.gif","Left");
+		newItem7.setData(HyperComposer.CMD_JUSTIFYLEFT);
+		newItem7.addSelectionListener(listener);
 		
-		ToolItem newItem17 = newItem(toolBar, "editor/incfont.gif","New");
-		ToolItem newItem18 = newItem(toolBar, "editor/decfont.gif","New");
+		ToolItem newItem8 = newItem(toolBar, "editor/aligncenter.gif","Center");
+		newItem8.setData(HyperComposer.CMD_JUSTIFYCENTER);
+		newItem8.addSelectionListener(listener);
+		
+		ToolItem newItem9 = newItem(toolBar, "editor/alignright.gif","Right");
+		newItem9.setData(HyperComposer.CMD_JUSTIFYRIGHT);
+		newItem9.addSelectionListener(listener);
+		ToolItem newItem10 = newItem(toolBar, "editor/alignjustify.gif","Justify");
+		newItem10.setData(HyperComposer.CMD_JUSTIFYWIDTH);
+		newItem10.addSelectionListener(listener);
+		
+		ToolItem newItem11 = newItem(toolBar, "editor/indent.gif","Indent");
+		newItem11.setData(HyperComposer.CMD_INDENT);
+		newItem11.addSelectionListener(listener);
+		
+		ToolItem newItem12 = newItem(toolBar, "editor/outdent.gif","Outdent");
+		newItem12.setData(HyperComposer.CMD_OUTDENT);
+		newItem12.addSelectionListener(listener);
+		
+		ToolItem newItem13 = newItem(toolBar, "editor/orderlist.gif","Orderlist");
+		newItem13.setData(HyperComposer.CMD_ORDERLIST);
+		newItem13.addSelectionListener(listener);
+		
+		ToolItem newItem14 = newItem(toolBar, "editor/unorderlist.gif","Unorderlist");
+		newItem14.setData(HyperComposer.CMD_UNORDERLIST);
+		newItem14.addSelectionListener(listener);
+		
+		ToolItem newItem15 = newItem(toolBar, "editor/subscript.gif","Subscript");
+		newItem15.setData(HyperComposer.CMD_SUBSCRIPT);
+		newItem15.addSelectionListener(listener);
+		ToolItem newItem16 = newItem(toolBar, "editor/superscript.gif","Superscript");
+		newItem16.setData(HyperComposer.CMD_SUPERSCRIPT);
+		newItem16.addSelectionListener(listener);
+		
+		ToolItem newItem17 = newItem(toolBar, "editor/incfont.gif","Inc font");
+		newItem17.setData(HyperComposer.CMD_INCREASEFONT);
+		newItem17.addSelectionListener(listener);
+		
+		ToolItem newItem18 = newItem(toolBar, "editor/decfont.gif","Dec font");
+		newItem18.setData(HyperComposer.CMD_DECREASEFONT);
+		newItem18.addSelectionListener(listener);
+		
 	    toolBar.pack();
 	    Point size = toolBar.getSize();
 	    CoolItem item = new CoolItem(coolBar, SWT.NONE);
@@ -117,12 +166,18 @@ public class HTMLEditor extends Composite {
 	    item.setPreferredSize(preferred);
 	    
 		composer = new HyperComposer(parentComposite, SWT.BORDER|SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
+		
 		FormData textData = new FormData();
 		textData.left = new FormAttachment(0);
 		textData.right = new FormAttachment(100);
 		textData.top = new FormAttachment(coolBar);
 		textData.bottom = new FormAttachment(100);
 
+		户口卡
+		购房合同前5页 15.
+		购房发票
+		居住证明
+		身份证。
 		composer.setLayoutData(textData);
 		composer.setLayout(new FillLayout());
 		composer.setFlavour(HyperComposer.FLAVOUR_CSS);
@@ -152,6 +207,7 @@ public class HTMLEditor extends Composite {
 			@Override
 			public void widgetSelected(SelectionEvent event) {
 				CTabItem item = (CTabItem)event.item;
+				logger.info("========" + state);
 				if(html.equals(item)&&state.equals(State.WYSWYG_MODIFY)){
 					logger.info("get content from composer");
 					styledText.setText(composer.getContent());
